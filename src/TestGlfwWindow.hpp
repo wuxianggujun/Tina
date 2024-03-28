12ec459e203eec7ac3380f48791e74323c11da88
wuxianggujun <3344207732@qq.com>
2024-03-28 14:07:37 +0800

    更新

--- src/TestGlfwWindow.hpp
+++ src/TestGlfwWindow.hpp
@@ -1,8 +1,26 @@
 #include "GlfwWindow.hpp"
+#include <filesystem>
+#include <fstream>
 
 class TestGlfwWindow : public Tina::GlfwWindow{
 
 public:
+
+	bgfx::ShaderHandle loadShader(const std::string& shaderPath) {
+		std::ifstream inputStream(shaderPath, std::ios::binary);
+		if (!inputStream.is_open())
+		{
+			throw std::runtime_error("Failed to open a shader file.");
+		}
+		inputStream.seekg(0, std::ios::end);
+		const long fileSize = inputStream.tellg();
+		inputStream.seekg(0);
+		const bgfx::Memory* mem = bgfx::alloc(fileSize + 1);
+		inputStream.read(reinterpret_cast<char*>(mem->data), fileSize);
+		mem->data[mem->size - 1] = '\0';
+		return bgfx::createShader(mem);
+
+	}
 
 
 	void render() {
