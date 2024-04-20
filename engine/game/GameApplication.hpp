fc1005b580f9fa8946f467c2ee18c5198a72ace0
wuxianggujun <3344207732@qq.com>
2024-04-20 22:29:50 +0800

    修改gameapplication文件

--- engine/game/GameApplication.hpp
+++ engine/game/GameApplication.hpp
@@ -18,8 +18,6 @@ namespace Tina {
         virtual void init(InitArgs initArgs) override;
         virtual bool update(float deltaTime) override;
         virtual void shutdown() override;
-
-        virtual Excution run() override;
     };
 
 
