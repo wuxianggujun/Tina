7503b8a056044ce0913a32683e48297cde62392e
wuxianggujun <3344207732@qq.com>
2024-04-25 15:12:00 +0800

    修改完善项目结构，舍弃之前的文件目录结构

--- cmake/TinaTool.cmake
+++ cmake/TinaTool.cmake
@@ -4,5 +4,5 @@ function(Group_Files_ByDirectory FILE_LIST)
     string(REPLACE "${CMAKE_CURRENT_SOURCE_DIR}/src" "" group_path "${file_path}")
     string(REPLACE "/" "\\" group_name "${group_path}")
     source_group("${group_name}" FILES "${file}")
-    endforeach
-endfunction
+    endforeach()
+endfunction()
