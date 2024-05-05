1c43e261d03c7892b34b5a9b010c03b03fe0d372
wuxianggujun <3344207732@qq.com>
2024-05-05 15:11:18 +0800

    遇到了问题，子目录下的依赖库会报错找不到头文件

--- engine/src/framework/log/Log.hpp
+++ engine/src/framework/log/Log.hpp
@@ -2,8 +2,21 @@
 #define TINA_FRAMEWORK_LOG_HPP
 
 #include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/logger.h>
+#include <spdlog/fmt/fmt.h>
+#include <spdlog/fmt/bundled/printf.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/daily_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/msvc_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/common.h>
+#include <spdlog/pattern_formatter.h>
 
 namespace Tina {
+    constexpr const char* LOG_PATH = "logs/default.log";
 
 
 
