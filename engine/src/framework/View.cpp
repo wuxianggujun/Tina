d73e5fbef7c6d2416f5484a20c54059a5949d69f
wuxianggujun <3344207732@qq.com>
2024-06-24 19:26:23 +0800

    删除无用子模块，同时添加yaml-cpp

---
+++ engine/src/framework/View.cpp
@@ -0,0 +1,8 @@
+//
+// Created by wuxianggujun on 2024/6/24.
+//
+
+#include "View.hpp"
+
+namespace Tina {
+} // Tina
---
+++ engine/src/framework/View.hpp
@@ -0,0 +1,16 @@
+//
+// Created by wuxianggujun on 2024/6/24.
+//
+
+#ifndef TINA_VIEW_HPP
+#define TINA_VIEW_HPP
+
+namespace Tina {
+
+    class View {
+
+    };
+
+} // Tina
+
+#endif //TINA_VIEW_HPP
