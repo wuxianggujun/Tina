9054e02627940a32ae40a855ba332f27c9ae6b8f
wuxianggujun <3344207732@qq.com>
2024-06-27 17:09:23 +0800

    更新并且必须设置TINA_AUTOUPDATE_SUBMODULE启用才会检查git版本是否大于2.35版本

--- engine/src/framework/View.hpp
+++ engine/src/framework/View.hpp
@@ -5,10 +5,28 @@
 #ifndef TINA_VIEW_HPP
 #define TINA_VIEW_HPP
 
+#include <iostream>
+#include <vector>
+#include <bgfx/bgfx.h>
+// #include "Layer.hpp"
+
+class Layer;
+
 namespace Tina {
 
     class View {
+    public:
+        View();
 
+        void addLayer(Layer *layer);
+
+        void submit();
+
+        static bgfx::ViewId nextViewId;
+
+    private:
+        bgfx::ViewId viewId;
+        std::vector<Layer> layers;
     };
 
 } // Tina
