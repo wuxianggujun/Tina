f2b644625e2516058bec87ce65f696d63279698e
wuxianggujun <3344207732@qq.com>
2024-07-20 16:53:23 +0800

    完善了项目的Vector类，准备将其他的类都删除

--- engine/src/math/Vector.hpp
+++ engine/src/math/Vector.hpp
@@ -37,24 +37,139 @@ namespace Tina
                 };
             };
 
-            glm::vec<2, T, glm::defaultp> internalVec2;
+            glm::vec<2, T, glm::defaultp> internalVec;
         };
 
-        Vector() : internalVec2(0, 0)
+        Vector() : internalVec(0, 0)
         {
         }
 
-        Vector(T x, T y) : internalVec2(x, y)
+        Vector(T x, T y) : internalVec(x, y)
         {
         }
 
-        explicit Vector(const glm::vec<2, T, glm::defaultp>& vec2) : internalVec2(vec2)
+        explicit Vector(const glm::vec<2, T, glm::defaultp>& vec2) : internalVec(vec2)
         {
+        }
+
+        T& operator[](const int index)
+        {
+            return (&x)[index];
+        }
+
+        const T& operator[](const int index) const
+        {
+            return (&x)[index];
+        }
+
+        Vector<T> operator+(const Vector<T>& other) const
+        {
+            return Vector<T>(internalVec + other.internalVec);
+        }
+
+        Vector<T> operator+(const T& other) const
+        {
+            return Vector<T>(internalVec.x + other, internalVec.y + other);
+        }
+
+        Vector<T> operator-(const Vector<T>& other) const
+        {
+            return Vector<T>(internalVec - other.internalVec);
+        }
+
+        Vector<T> operator-(const T& other) const
+        {
+            return Vector<T>(internalVec.x - other, internalVec.y - other);
+        }
+
+        Vector<T> operator*(const Vector<T>& other) const
+        {
+            return Vector<T>(internalVec * other.internalVec);
+        }
+
+
+        Vector<T> operator*(const T& other) const
+        {
+            return Vector<T>(internalVec.x * other, internalVec.y * other);
+        }
+
+        Vector<T> operator/(const Vector<T>& other) const
+        {
+            return Vector<T>(internalVec / other.internalVec);
+        }
+
+        Vector<T> operator/(const T& other) const
+        {
+            return Vector<T>(internalVec.x / other, internalVec.y / other);
+        }
+
+        Vector<T>& operator+=(const Vector<T>& other)
+        {
+            internalVec += other.internalVec;
+            return *this;
+        }
+
+        Vector<T>& operator+=(const T& other)
+        {
+            internalVec.x += other;
+            internalVec.y += other;
+            return *this;
+        }
+
+        bool operator==(const Vector& other) const
+        {
+            return internalVec == other.internalVec;
+        }
+
+
+        bool operator!=(const Vector& other) const
+        {
+            return internalVec != other.internalVec;
+        }
+
+
+        Vector<T>& operator-=(const Vector<T>& other)
+        {
+            internalVec -= other.internalVec;
+            return *this;
+        }
+
+        Vector<T>& operator-=(const T& other)
+        {
+            internalVec.x -= other;
+            internalVec.y -= other;
+            return *this;
+        }
+
+        Vector<T>& operator*=(const Vector<T>& other)
+        {
+            internalVec *= other.internalVec;
+            return *this;
+        }
+
+        bool operator<(const Vector<T>& other) const
+        {
+            return internalVec.x < other.internalVec.x && internalVec.y < other.internalVec.y;
+        }
+
+        bool operator>(const Vector<T>& other) const
+        {
+            return internalVec.x > other.internalVec.x && internalVec.y > other.internalVec.y;
+        }
+
+        bool isZero() const
+        {
+            return x == 0 && y == 0;
+        }
+
+        bool length() const
+        {
+            return std::sqrt(x * x + y * y);
         }
 
         [[nodiscard]] std::string toString() const
         {
-            return std::format("[ x: %s, y: %s ]", this->x, this->y);
+            return std::format("[ x: {}, y: {} ]", this->x, this->y);
         }
 
         void toStdout() const
