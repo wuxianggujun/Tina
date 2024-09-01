c1523f71f9521e16f46e5e5fd5e8c0190e314c64
wuxianggujun <3344207732@qq.com>
2024-09-01 21:49:03 +0800

    添加几个类，防止重复造轮子

--- engine/src/filesystem/File.hpp
+++ engine/src/filesystem/File.hpp
@@ -1,10 +1,10 @@
 #ifndef TINA_FILESYSTEM_FILE_HPP
 #define TINA_FILESYSTEM_FILE_HPP
 
+#include <cstdarg>
 #include <vector>
 
 #include "Path.hpp"
-#include "FileStream.hpp"
 #include "io/Closeable.hpp"
 
 namespace Tina
@@ -18,6 +18,116 @@ namespace Tina
         Append = 1 << 4
     };
 
+    class FileStream
+    {
+    private:
+        FILE* file = nullptr;
+
+    public:
+        FileStream();
+
+        ~FileStream()
+        {
+            if (file)
+            {
+                fclose(file);
+            }
+        }
+
+        explicit FileStream(FILE* file): file(file)
+        {
+        }
+
+        FileStream(const char* path, const char* mode)
+        {
+            file = fopen(path, mode);
+        }
+
+        void reopen(const char* path, const char* mode) const
+        {
+            freopen(path, mode, file);
+        }
+
+        void seek(const long offset, const int origin) const
+        {
+            fseek(file, offset, origin);
+        }
+
+        size_t read(void* ptr, const size_t itemSIze, const size_t itemCount) const
+        {
+            return fread(ptr, itemSIze, itemCount, file);
+        }
+
+        size_t write(const void* ptr, const size_t itemSize, const size_t itemCount) const
+        {
+            return fwrite(ptr, itemSize, itemCount, file);
+        }
+
+        [[nodiscard]] long tell() const
+        {
+            return ftell(file);
+        }
+
+        [[nodiscard]] size_t close() const
+        {
+            return fclose(file);
+        }
+
+        [[nodiscard]] size_t putChar(const char c) const
+        {
+            return fputc(c, file);
+        }
+
+        [[nodiscard]] size_t getChar() const
+        {
+            return fgetc(file);
+        }
+
+        size_t scanf(const char* format, ...) const
+        {
+            va_list args;
+            va_start(args, format);
+            return vfscanf(file, format, args);
+        }
+
+        size_t printf(const char* format, ...) const
+        {
+            va_list args;
+            va_start(args, format);
+            return vfprintf(file, format, args);
+        }
+
+        [[nodiscard]] bool eof() const
+        {
+            return feof(file);
+        }
+
+        [[nodiscard]] size_t flush() const
+        {
+            return fflush(file);
+        }
+
+        [[nodiscard]] size_t error() const
+        {
+            return ferror(file);
+        }
+
+        void clearError() const
+        {
+            ::clearerr(file);
+        }
+
+        void rewind() const
+        {
+            ::rewind(file);
+        }
+
+        [[nodiscard]] FILE* getFile() const
+        {
+            return file;
+        }
+    };
+    
     class File : Closeable
     {
     public:
