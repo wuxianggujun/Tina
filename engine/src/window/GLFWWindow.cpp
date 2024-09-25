9497751c6b93af1506c67170bab5696cffc55a16
wuxianggujun <3344207732@qq.com>
2024-09-26 00:09:55 +0800

    代码没有问题可以在Ubuntu上运行编译

--- engine/src/window/GLFWWindow.cpp
+++ engine/src/window/GLFWWindow.cpp
@@ -6,6 +6,8 @@
 
 #include <fmt/printf.h>
 
+#include "bgfx/platform.h"
+
 namespace Tina {
     GLFWWindow::GLFWWindow() : m_window(nullptr, GlfwWindowDeleter()) {
         glfwSetErrorCallback(errorCallback);
@@ -14,13 +16,13 @@ namespace Tina {
             return;
         }
     }
-    
-      void GLFWWindow::create(WindowConfig config) {
-/*#if BX_PLATFORM_LINUX
+
+    void GLFWWindow::create(WindowConfig config) {
+#if BX_PLATFORM_LINUX
         glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
         /*if (glfwPlatformSupported(GLFW_PLATFORM_WAYLAND))
-            glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);#1#
-#endif*/
+            glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);*/
+#endif
 
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         m_window.reset(glfwCreateWindow(config.size.width, config.size.height, config.title, nullptr, nullptr));
@@ -35,15 +37,22 @@ namespace Tina {
         bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
         bgfxInit.callback = &m_bgfxCallback;
 
-#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-        bgfxInit.platformData.nwh = (void*)(uintptr_t) glfwGetX11Window(m_window.get());
-        bgfxInit.platformData.ndt = glfwGetX11Display();
-#elif BX_PLATFORM_OSX
-        bgfxInit.platformData.nwh = glfwGetCocoaWindow(m_window.get());
-#elif BX_PLATFORM_WINDOWS
-        bgfxInit.platformData.nwh = glfwGetWin32Window(m_window.get());
+        bgfx::PlatformData pd;
+        pd.context = glfwGetCurrentContext();
+#if defined(GLFW_EXPOSE_NATIVE_WAYLAND)
+        pd.nwh = glfwGetWaylandWindow(m_window.get());
+        pd.ndt = glfwGetWaylandDisplay();
+#elif defined(GLFW_EXPOSE_NATIVE_X11)
+        pd.nwh = (void *) (uintptr_t) glfwGetX11Window(m_window.get());
+        pd.ndt = glfwGetX11Display();
+#elif defined(GLFW_EXPOSE_NATIVE_WIN32)
+        pd.nwh = glfwGetWin32Window(m_window.get());
+#elif defined(GLFW_EXPOSE_NATIVE_COCOA)
+        pd.nwh = glfwGetCocoaWindow(m_window.get());
 #endif
 
+        bgfxInit.platformData = pd;
+        
         if (!bgfx::init(bgfxInit)) {
             fmt::printf("Bgfx initialization failed\n");
             return;
